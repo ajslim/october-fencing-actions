e1a85cf4d633a7a2424efaeaa7af0583ff04504f
ajslim <ajslim@gmail.com>
2020-04-30 15:38:57 +0100

    * Fix by one queries

--- api/ByOne.php
+++ api/ByOne.php
@@ -57,56 +57,69 @@ class ByOne extends Api
     {
         $results = DB::select( DB::raw(
             "Select
-                fencer_id as id, 
-                  last_name, 
-                  first_name, 
-                  by_one_wins, 
-                  by_one_losses, 
-                  (by_one_wins + by_one_losses) as total_by_one, 
-                  (by_one_wins / (by_one_wins + by_one_losses)) as by_one_win_percent, 
-                  (left_total_bouts + right_total_bouts) as total_bouts, 
-                  (by_one_wins + by_one_losses) / (left_total_bouts + right_total_bouts) as by_one_percent
+                fencer_id as id,
+                last_name,
+                first_name,
+                by_one_wins,
+                by_one_losses,
+                (by_one_wins + by_one_losses) as total_by_one,
+                (by_one_wins / (by_one_wins + by_one_losses)) as by_one_win_percent,
+                (left_total_bouts + right_total_bouts) as total_bouts,
+                (by_one_wins + by_one_losses) / (left_total_bouts + right_total_bouts) as by_one_percent
             From
             (
                 Select fencer_id, last_name, first_name, by_one_wins, by_one_losses, left_total_bouts, right_total_bouts from
                 (
                     SELECT fencer_id, COALESCE(left_bouts, 0) as left_total_bouts, COALESCE(right_bouts, 0) as right_total_bouts, by_one_wins, by_one_losses FROM
                     (
-                        select fencer_id, count(fencer_id) as by_one_wins from
-                        (
-                            (SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1)
-                            union
-                            (SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1)
-                        ) as by_one_wins_select
-                        group by fencer_id
-                    ) as by_one_wins_select_total
-            
+						SELECT
+							fencers.id as fencer_id,
+							count(fencer_bouts.fencer_id) as by_one_wins
+						FROM
+							october_business.ajslim_fencingactions_fencers as fencers
+						LEFT JOIN
+							(
+								(SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1)
+								union
+								(SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1)
+							) as fencer_bouts
+						ON fencers.id = fencer_bouts.fencer_id
+						GROUP BY fencers.id
+					) as by_one_wins_select_total
+
                     join
-            
+
                     (
-                        select fencer_id as loss_fencer_id, count(fencer_id) as by_one_losses from
-                        (
-                            (SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1)
-                            union
-                            (SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1)
-                        ) as by_one_wins_select
-                        group by loss_fencer_id
-                    ) as by_one_loss_select_total
-            
+						SELECT
+							fencers.id as loss_fencer_id,
+							count(fencer_bouts.fencer_id) as by_one_losses
+						FROM
+							october_business.ajslim_fencingactions_fencers as fencers
+						LEFT JOIN
+							(
+								(SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1)
+								union
+								(SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1)
+							) as fencer_bouts
+						ON fencers.id = fencer_bouts.fencer_id
+						GROUP BY fencers.id
+					) as by_one_losses_select_total
+
                     on fencer_id = loss_fencer_id
-            
-                    left join
+
+                    left outer join
                     (SELECT left_fencer_id, count(left_fencer_id) as left_bouts FROM october_business.ajslim_fencingactions_bouts group by left_fencer_id) as L
                     on fencer_id = left_fencer_id
-            
-                    left join
+
+                    left outer join
                     (SELECT right_fencer_id, count(right_fencer_id) as right_bouts FROM october_business.ajslim_fencingactions_bouts group by right_fencer_id) as R
                     on fencer_id = right_fencer_id
                 ) as X
                 join
                     october_business.ajslim_fencingactions_fencers
                 on ajslim_fencingactions_fencers.id = fencer_id
-            ) as Y"
+            ) as Y
+            order by by_one_win_percent desc"
         ));
 
 
@@ -145,56 +158,69 @@ class ByOne extends Api
     {
         $results = DB::select( DB::raw(
             "Select
-                fencer_id as id, 
-                  last_name, 
-                  first_name, 
-                  by_one_wins, 
-                  by_one_losses, 
-                  (by_one_wins + by_one_losses) as total_by_one, 
-                  (by_one_wins / (by_one_wins + by_one_losses)) as by_one_win_percent, 
-                  (left_total_bouts + right_total_bouts) as total_bouts, 
-                  (by_one_wins + by_one_losses) / (left_total_bouts + right_total_bouts) as by_one_percent
+	            fencer_id as id,
+                last_name,
+                first_name,
+                by_one_wins,
+                by_one_losses,
+                (by_one_wins + by_one_losses) as total_by_one,
+                (by_one_wins / (by_one_wins + by_one_losses)) as by_one_win_percent,
+                (left_total_bouts + right_total_bouts) as total_bouts,
+                (by_one_wins + by_one_losses) / (left_total_bouts + right_total_bouts) as by_one_percent
             From
             (
                 Select fencer_id, last_name, first_name, by_one_wins, by_one_losses, left_total_bouts, right_total_bouts from
                 (
                     SELECT fencer_id, COALESCE(left_bouts, 0) as left_total_bouts, COALESCE(right_bouts, 0) as right_total_bouts, by_one_wins, by_one_losses FROM
                     (
-                        select fencer_id, count(fencer_id) as by_one_wins from
-                        (
-                            (SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1 and (left_score > 5 or right_score > 5))
-                            union
-                            (SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1 and (left_score > 5 or right_score > 5))
-                        ) as by_one_wins_select
-                        group by fencer_id
-                    ) as by_one_wins_select_total
-            
+						SELECT
+							fencers.id as fencer_id,
+							count(fencer_bouts.fencer_id) as by_one_wins
+						FROM
+							october_business.ajslim_fencingactions_fencers as fencers
+						LEFT JOIN
+							(
+								(SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1 and (left_score > 5 or right_score > 5))
+								union
+								(SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1 and (left_score > 5 or right_score > 5))
+							) as fencer_bouts
+						ON fencers.id = fencer_bouts.fencer_id
+						GROUP BY fencers.id
+					) as by_one_wins_select_total
+
                     join
-            
+
                     (
-                        select fencer_id as loss_fencer_id, count(fencer_id) as by_one_losses from
-                        (
-                            (SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1 and (left_score > 5 or right_score > 5))
-                            union
-                            (SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1 and (left_score > 5 or right_score > 5))
-                        ) as by_one_wins_select
-                        group by loss_fencer_id
-                    ) as by_one_loss_select_total
-            
+						SELECT
+							fencers.id as loss_fencer_id,
+							count(fencer_bouts.fencer_id) as by_one_losses
+						FROM
+							october_business.ajslim_fencingactions_fencers as fencers
+						LEFT JOIN
+							(
+								(SELECT id, right_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where left_score - right_score = 1 and (left_score > 5 or right_score > 5))
+								union
+								(SELECT id, left_fencer_id as fencer_id FROM october_business.ajslim_fencingactions_bouts where right_score - left_score = 1 and (left_score > 5 or right_score > 5))
+							) as fencer_bouts
+						ON fencers.id = fencer_bouts.fencer_id
+						GROUP BY fencers.id
+					) as by_one_losses_select_total
+
                     on fencer_id = loss_fencer_id
-            
-                    left join
+
+                    left outer join
                     (SELECT left_fencer_id, count(left_fencer_id) as left_bouts FROM october_business.ajslim_fencingactions_bouts where (left_score > 5 or right_score > 5) group by left_fencer_id) as L
                     on fencer_id = left_fencer_id
-            
-                    left join
+
+                    left outer join
                     (SELECT right_fencer_id, count(right_fencer_id) as right_bouts FROM october_business.ajslim_fencingactions_bouts where (left_score > 5 or right_score > 5) group by right_fencer_id) as R
                     on fencer_id = right_fencer_id
                 ) as X
                 join
                     october_business.ajslim_fencingactions_fencers
                 on ajslim_fencingactions_fencers.id = fencer_id
-            ) as Y"
+            ) as Y
+            order by by_one_win_percent desc"
         ));
 
 
